f54dec3b5baea8798abf639f5f2302bea79494e7
Alef A. Araujo <3alyef.dev@gmail.com>
2024-03-15 18:05:37 -0300

    start

---
+++ src/components/Body/Body.component.tsx
@@ -0,0 +1,10 @@
+const Body = ()=>{
+    return(
+        <>
+        
+        
+        </>
+    )
+}
+
+export { Body };
---
+++ src/components/components.ts
@@ -0,0 +1,5 @@
+import { Header } from "./Header/Header.component";
+import { Body } from "./Body/Body.component";
+import { Footer } from "./Footer/Footer.component";
+
+export { Header, Body, Footer}
