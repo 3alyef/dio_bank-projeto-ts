7748387a118c8cc694bf9419c318d08c9ddb2118
Alef A. Araujo <3alyef.dev@gmail.com>
2024-03-19 21:39:07 -0300

    update

--- src/components/main/page.tsx
+++ src/components/main/page.tsx
@@ -4,7 +4,7 @@ export const Main = ()=>{
     
     return(
         <main className="flex flex-col items-center min-h-[80vh] justify-center">
-
+            
             <RoutesView/>
             
         </main>
